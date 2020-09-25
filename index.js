e93d23d9740f11da8075cc6514c5559a9ba718bc
Loc Mai <lnmai@ucdavis.edu>
2020-09-24 22:09:01 -0700

    clear interval if API not working

--- index.js
+++ index.js
@@ -13,24 +13,30 @@ const interval = setInterval(function () {
   fetch('https://api-prod.nvidia.com/direct-sales-shop/DR/products/en_us/USD/5438481600,5438481700')
     .then((res) => res.json())
     .then((res) => {
-      const products = res.products.product;
-      products.forEach((product) => {
-        if (product.inventoryStatus.status === 'PRODUCT_INVENTORY_IN_STOCK') {
-          notifier.notify({
-            title: product.displayName,
-            message: 'IN STOCK',
-            icon: './rtx3000.png',
-            sound: true, // Only Notification Center or Windows Toasters
-            wait: true, // Wait with callback, until user action is taken against notification, does not apply to Windows Toasters as they always wait or notify-send as it does not support the wait option
-          });
-          console.log(chalk.green(`${product.displayName} - IN STOCK`));
-          console.log(chalk.green(`Opening link: ${PROPS[product.id]}`));
-          open(PROPS[product.id]);
-          playSound();
-        } else {
-          console.log(chalk.red(`${product.displayName} - OUT OF STOCK`));
-        }
-      });
+      try {
+        const products = res.products.product;
+        products.forEach((product) => {
+          if (product.inventoryStatus.status === 'PRODUCT_INVENTORY_IN_STOCK') {
+            notifier.notify({
+              title: product.displayName,
+              message: 'IN STOCK',
+              icon: './rtx3000.png',
+              sound: true, // Only Notification Center or Windows Toasters
+              wait: true, // Wait with callback, until user action is taken against notification, does not apply to Windows Toasters as they always wait or notify-send as it does not support the wait option
+            });
+            console.log(chalk.green(`${product.displayName} - IN STOCK`));
+            console.log(chalk.green(`Opening link: ${PROPS[product.id]}`));
+            open(PROPS[product.id]);
+            playSound();
+          } else {
+            console.log(chalk.red(`${product.displayName} - OUT OF STOCK`));
+          }
+        });
+      } catch (err) {
+        console.log(err);
+        console.log(chalk.red('API seems dead'));
+        clearInterval(interval);
+      }
     });
 }, 5000);
 
